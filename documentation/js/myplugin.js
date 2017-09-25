f3b7bd642580c85c8a89e560d8f367921d10122d
smontoro <sarahjmontoro@gmail.com>
2017-09-24 17:04:33 -0700

    updated theme

---
+++ documentation/js/myplugin.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    /**************trigger nice scroll**************/
+    $("html").niceScroll({
+
+    });
+    /**************end trigger nice scroll**************/
+
+})
